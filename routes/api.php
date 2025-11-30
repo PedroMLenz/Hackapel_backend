7fd9eaf7ecda6cc12f3a019742d3e7a8437f571e
matheusaguiarrr <matheustitanio5@gmail.com>
2025-11-30 12:20:27 -0300

    fix: correct route definitions for login and notifications in API

--- routes/api.php
+++ routes/api.php
@@ -8,9 +8,9 @@ use App\Http\Controllers\InformationController;
 use App\Http\Controllers\ScheduleController;
 use App\Http\Controllers\UserController;
 
-Route::post('/login', [AuthController::class, 'login']);
-Route::post('/users/{id}/informations', [InformationController::class, 'store'])->middleware('auth:sanctum');
-Route::get('/users/{id}/informations', [InformationController::class, 'indexByUser'])->middleware('auth:sanctum');
+Route::post('/login', action: [AuthController::class, 'login']);
+Route::post('/users/{id}/notifications', [InformationController::class, 'store'])->middleware('auth:sanctum');
+Route::get('/users/{id}/notifications', [InformationController::class, 'indexByUser'])->middleware('auth:sanctum');
 Route::get('/filters', [FilterController::class, 'index'])->middleware('auth:sanctum');
 Route::post('/telegram/webhook', [InformationController::class, 'handleTelegramWebhook']);
 Route::post('/users/{id}/informations', [UserController::class, 'store'])->middleware('auth:sanctum');
